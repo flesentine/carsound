3825cd610d97d9b6101baa60df1f8a60f09ec195
flesentine <chrisgmcc@gmail.com>
2026-09-25 11:36:27 -0700

    Wire microphone capture into app state

--- QuietDriveLab/App/QuietDriveLabApp.swift
+++ QuietDriveLab/App/QuietDriveLabApp.swift
@@ -4,12 +4,14 @@ import SwiftUI
 struct QuietDriveLabApp: App {
     @State private var microphonePermission = MicrophonePermissionModel()
     @State private var audioSession = AudioSessionModel()
+    @State private var microphoneCapture = MicrophoneCaptureModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(microphonePermission)
                 .environment(audioSession)
+                .environment(microphoneCapture)
                 .task {
                     microphonePermission.refresh()
                     audioSession.refreshRoute()
